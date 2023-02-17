9f8b3bec4cc09cb935645fc5c166cd471f9eef26
Adrien Brault <adrien.brault@gmail.com>
2023-02-17 13:20:04 +0100

    fix: client namespace

--- src/Client.php
+++ src/Client.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Sourceability\OpenaiClient;
+namespace Sourceability\OpenAIClient;
 
 use Http\Client\Common\Plugin\AddHostPlugin;
 use Http\Client\Common\Plugin\AddPathPlugin;
